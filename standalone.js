eb4085208422461f0e353ee56d107b8a7be36fe9
Manuel Spigolon <manuel.spigolon@nearform.com>
2022-08-22 09:29:46 +0200

    chore: unuseful if

--- standalone.js
+++ standalone.js
@@ -23,10 +23,8 @@ function StandaloneSerializer (options = { readMode: true }) {
   // WRITE MODE: it behalf on the default SerializerSelector, wrapping the API to run the Ajv Standalone code generation
   const factory = SerializerSelector()
   return function wrapper (externalSchemas, serializerOpts = {}) {
-    if (!serializerOpts.mode || serializerOpts.mode !== 'standalone') {
-      // to generate the serialization source code, these options are mandatory
-      serializerOpts.mode = 'standalone'
-    }
+    // to generate the serialization source code, this option is mandatory
+    serializerOpts.mode = 'standalone'
 
     const compiler = factory(externalSchemas, serializerOpts)
     return function (opts) { // { schema/*, method, url, httpPart */ }
